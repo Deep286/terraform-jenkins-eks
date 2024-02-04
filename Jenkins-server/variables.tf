657ddbe3adf7dcec40cb346c6e8559dba5b6d28f
Deep286 <deepsuthar286@gmail.com>
2024-02-04 11:05:23 +0530

    adding files to repo

---
+++ Jenkins-server/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "vpc cidr"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "subnets cidr"
+  type        = list(string)
+}
+
+variable "instance_type" {
+    description = "Instance type"
+    type = string  
+}
